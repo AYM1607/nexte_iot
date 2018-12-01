cdc3f41a6bc19987c48f211925c9198d26f9b043
Mariano <marianok300@gmail.com>
2018-12-01 22:31:15 +0000

    Tried to fix communication errors

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
+import logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from pymodbus.register_read_message import ReadInputRegistersResponse
 
-client = ModbusClient(method="rtu", port="/dev/ttyUSB0", stopbits=1, bytesize=8, parity="E", baudrate=9600, timeout=0.3)
+client = ModbusClient(method="rtu", port="/dev/ttyUSB0", stopbits=1, bytesize=8, parity="E", baudrate=9600, timeout=3)
 
 connection = client.connect()
 
 print(connection)
 
-value = client.read_input_registers(48, 2, unit=0x01)
+result = client.read_holding_registers(48, 1, unit=0x01)
 
-print(value)
-print(value.register)
+print(result)
+print(result.raw)
 
 client.close()
