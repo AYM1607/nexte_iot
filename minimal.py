71d7c491c2b2384e646082d57af89b0adc06c8e2
Mariano <marianok300@gmail.com>
2018-12-01 23:42:22 +0000

    Communication working between raspberry and meter

--- minimal.py
+++ minimal.py
@@ -3,8 +3,12 @@ import serial
 
 instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
 
-instrument.serial.baudrate = 9600
+instrument.serial.baudrate = 4800
 instrument.serial.parity = serial.PARITY_EVEN
+instrument.serial.timeout = 3
 
-print(instrument)
+# print(instrument)
 
+voltage = instrument.read_float(48, functioncode=3, numberOfRegisters=2)
+
+print(voltage)
