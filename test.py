d35c87c18da0b7ec76b1426b3eb587680d46eda1
Mariano Uvalle <marianok300@gmail.com>
2018-12-01 15:21:31 -0600

    Added test code to read the voltage register

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hello")
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.register_read_message import ReadInputRegisterResponse
+
+client = ModbusClient(method="rtu", port="/dev/ttyUSB0", stopbits=1, bytesize=8, parity="E", baudrate=9600, timeout=0.3)
+
+connection = client.connect()
+
+print(connection)
+
+value = client.read_input_register(48, 2, unit=0x01)
+
+print(value)
+print(value.register)
+
+client.close()
